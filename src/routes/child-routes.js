ee1c73d89b2ee51314b527cce40154748bc0efce
osammot10 <tommasopellizzon2000@gmail.com>
2021-12-07 15:48:41 +0100

    bug fixed

--- src/routes/child-routes.js
+++ src/routes/child-routes.js
@@ -55,37 +55,37 @@ router.get('/', (req, res, next) => {
 })
 
 // TODO: Insert a label
-router.post('/label/:childId', (req, res, next) => {
-	if (!req.user_id) { return res.status(401).send('Not authenticated') }
-	// const { ids } = req.query
-	// if (!ids){
-	// 	return res.status(400).send('Bad Request')
-	// }
+// router.post('/label/:childId', (req, res, next) => {
+// 	if (!req.user_id) { return res.status(401).send('Not authenticated') }
+// 	// const { ids } = req.query
+// 	// if (!ids){
+// 	// 	return res.status(400).send('Bad Request')
+// 	// }
 
-	let labelName = req.body.label
-	if(!labelName)
-		return res.status(400).send('Bad Request')
-	Label.findOne({labelName}).exec().then((l) => {
-		if(l){
-			const newLabel = { name }
-		}
-		else{
-			return res.status(400).send('Label does not exists')
-		}
-		try{
-			await Label.create(newLabel)
-			res.status(200).send('Label created')
-		}
-		catch (error) {
-    next(error)
-  	}
-	}).catch((error) => {
-		return res.status(400).send('Impossibile to retrieve label')
-	})
-})
+// 	let labelName = req.body.label
+// 	if(!labelName)
+// 		return res.status(400).send('Bad Request')
+// 	Label.findOne({labelName}).exec().then((l) => {
+// 		if(l){
+// 			const newLabel = { name }
+// 		}
+// 		else{
+// 			return res.status(400).send('Label does not exists')
+// 		}
+// 		try{
+// 			Label.create(newLabel)
+// 			res.status(200).send('Label created')
+// 		}
+// 		catch (error) {
+//     next(error)
+//   	}
+// 	}).catch((error) => {
+// 		return res.status(400).send('Impossibile to retrieve label')
+// 	})
+// })
 
 // TODO: Get all child labels
-router.get
+// router.get
 
 // TODO: Delete a label
 
