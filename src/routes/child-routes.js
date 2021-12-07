5b669c97d2a24289e0d41038685cced75c881170
GabrielRosace <gabrielrosace@hotmail.it>
2021-12-07 13:58:41 +0100

    Added parent information into children

--- src/routes/child-routes.js
+++ src/routes/child-routes.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = new express.Router()
 
 const Child = require('../models/child')
+const Profile = require('../models/profile')
+const Parent = require('../models/parent')
 
 router.get('/', (req, res, next) => {
   if (!req.user_id) { return res.status(401).send('Not authenticated') }
@@ -9,17 +11,48 @@ router.get('/', (req, res, next) => {
   if (!ids) {
     return res.status(400).send('Bad Request')
   }
+
+  //* Old function
+  // Child.find({ child_id: { $in: ids } })
+  //   .select('given_name family_name image_id child_id suspended')
+  //   .populate('image')
+  //   .lean()
+  //   .exec()
+  //   .then(profiles => {
+  //     if (profiles.length === 0) {
+  //       return res.status(404).send('Children not found')
+  //     }
+  //     Parent.findOne({ child_id: profiles})
+  //     res.json(profiles)
+  //   }).catch(next)
+
+
+  //? Si può fare meglio
   Child.find({ child_id: { $in: ids } })
-    .select('given_name family_name image_id child_id suspended')
-    .populate('image')
-    .lean()
-    .exec()
-    .then(profiles => {
-      if (profiles.length === 0) {
-        return res.status(404).send('Children not found')
+  .select('given_name family_name image_id child_id suspended')
+  .lean()
+  .populate('image')
+  .populate('parent')
+  .exec()
+  .then((profiles) => {
+    if (profiles.length === 0) {
+    return res.status(404).send('Childen not found')
+    }
+
+    profiles.forEach(async (value,index) => {
+      value.parent = value.parent.parent_id
+
+      const parentprofile = await Profile.findOne({ user_id: value.parent }, 'user_id given_name family_name image_id').lean().populate('image')
+
+      value.parent = parentprofile
+      console.log(`${index}`)
+      if (index == profiles.length-1) {
+        res.json(profiles)
       }
-      res.json(profiles)
-    }).catch(next)
+    })
+  }).catch(next)
 })
+
+
 
 module.exports = router
