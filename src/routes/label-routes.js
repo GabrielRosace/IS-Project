cb038db96252d0343341353568777ca8e37fb2c1
osammot10 <tommasopellizzon2000@gmail.com>
2021-12-07 17:12:53 +0100

    labels getter added

--- src/routes/label-routes.js
+++ src/routes/label-routes.js
@@ -3,16 +3,26 @@ const router = new express.Router()
 
 const Label = require('../models/label')
 const Group = require('../models/group')
+const Member = require('../models/member')
 const objectid = require('objectid')
 
 // Get all labels of the group 
 router.get('/', (req, res, next) => {
     if (!req.user_id) { return res.status(401).send('Not authenticated') }
-
-	Label.find({}).exec().then((l) => {
-        res.status(200).send(l)
-	}).catch((error) => {
-		return res.status(400).send('Impossibile to retrieve label')
+	// let userId = req.user_id
+	let groupId = req.body.group_id
+	Member.find({group_id : groupId}).then((m) => {
+		// console.log(m.group_id)
+		if(m){
+			Label.find({group_id : groupId}).exec().then((l) => {
+				res.status(200).send(l)
+			}).catch((error) => {
+				return res.status(400).send('Impossibile to retrieve label')
+			})
+		}
+		else{
+			console.log("It is not a member of any group".red);
+		}
 	})
 })
 
@@ -66,5 +76,8 @@ router.post('/', (req, res, next) => {
 })
 
 // TODO delete a label
+router.delete('/', (req, res, next) => {
+	
+})
 
 module.exports = router
