a2b4f86a4423559989728344d55ee4d0448aca0f
elle03 <ellero.alberto@gmail.com>
2021-12-15 13:25:30 +0100

    fix bugs create new event

--- Nekoma_FamiliesShare/app/src/main/java/com/example/nekoma_families_share/Creazione_evento.java
+++ Nekoma_FamiliesShare/app/src/main/java/com/example/nekoma_families_share/Creazione_evento.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AlertDialog;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -120,134 +121,134 @@ public class Creazione_evento extends AppCompatActivity  {
         String dataFine = ((EditText)findViewById(R.id.inputEventEndEvent)).getText().toString();
         String inizio = ((EditText) findViewById(R.id.inputEventStartTime)).getText().toString();
         String fine = ((EditText) findViewById(R.id.inputEventEndTime)).getText().toString();
+        if(nome.equals("") || luogo.equals("") || desc.equals("") || dataInizio.equals("") || inizio.equals("") ||dataFine.equals("") || fine.equals("")){
+            Toast.makeText(Creazione_evento.this, "INSERIRE TUTTI I CAMPI", Toast.LENGTH_LONG).show();
+        }else{
+            SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+            SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
 
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-        SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            Date d = format.parse(dataInizio+" "+inizio);
+            String s = format2.format(d);
+            Date DInizio = format2.parse(s);
 
-        Date d = format.parse(dataInizio+" "+inizio);
-        String s = format2.format(d);
-        Date DInizio = format2.parse(s);
-
-        d = format.parse(dataFine+" "+fine);
-        s = format2.format(d);
-        Date DFine = format2.parse(s);
-        System.out.println(DInizio + " " +DFine);
-        String DateEnd = format2.format(DFine);
-        String DateStart = format2.format(DInizio);
-        System.out.println(DateStart + " " +DateEnd);
-        String labels = "[";
-        Iterator i = labelSelect.iterator();
-        boolean control = true;
-        while(i.hasNext()){
-            Button b = (Button) i.next();
-            int buttonColor = ((ColorDrawable) b.getBackground()).getColor();
-            if(buttonColor == Color.GREEN) {
-                for (MyEtichette m:etichette) {
-                    if(m.getName().equals(b.getText().toString())){
-                        if(control){
-                            labels = labels = labels + m.getId();
-                            control = false;
-                        }else
-                            labels = labels + "," + m.getId();
+            d = format.parse(dataFine+" "+fine);
+            s = format2.format(d);
+            Date DFine = format2.parse(s);
+            System.out.println(DInizio + " " +DFine);
+            String DateEnd = format2.format(DFine);
+            String DateStart = format2.format(DInizio);
+            System.out.println(DateStart + " " +DateEnd);
+            String labels = "[";
+            Iterator i = labelSelect.iterator();
+            boolean control = true;
+            while(i.hasNext()){
+                Button b = (Button) i.next();
+                int buttonColor = ((ColorDrawable) b.getBackground()).getColor();
+                if(buttonColor == Color.GREEN) {
+                    for (MyEtichette m:etichette) {
+                        if(m.getName().equals(b.getText().toString())){
+                            if(control){
+                                labels = labels = labels + m.getId();
+                                control = false;
+                            }else
+                                labels = labels + "," + m.getId();
+                        }
                     }
                 }
             }
-        }
-        labels = labels + "]";
-        System.out.println("----->"+labels);
+            labels = labels + "]";
+            System.out.println("----->"+labels);
 
-        HashMap<String,String> map = new HashMap<>();
-        String id_group = Utilities.getPrefs(this).getString("group", "");
-        map.put("group_id",id_group);
-        map.put("creator_id", Utilities.getUserID(Creazione_evento.this));
-        map.put("name", nome);
-        map.put("color","#000000");
-        map.put("description",desc);
-        map.put("location", luogo);
-        map.put("repetition","false");
-        map.put("repetition_type", "");
-        map.put("different_timeslots","false");
-        map.put("labels",labels);
-        map.put("status", "accepted");
-        map.put("activity_id","");
-        map.put("imgUrl","");
+            HashMap<String,String> map = new HashMap<>();
+            String id_group = Utilities.getPrefs(this).getString("group", "");
+            map.put("group_id",id_group);
+            map.put("creator_id", Utilities.getUserID(Creazione_evento.this));
+            map.put("name", nome);
+            map.put("color","#000000");
+            map.put("description",desc);
+            map.put("location", luogo);
+            map.put("repetition","false");
+            map.put("repetition_type", "");
+            map.put("different_timeslots","false");
+            map.put("labels",labels);
+            map.put("status", "accepted");
+            map.put("activity_id","");
+            map.put("imgUrl","");
 
-        Utilities.httpRequest(this, Request.Method.GET, "/groups?searchBy=ids&ids=" + id_group, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    JSONArray user_response = new JSONArray((String) response);
-                    map.put("group_name",new JSONObject(user_response.getString(0)).getString("name"));
-                    System.out.println("----->"+user_response);
-                } catch (JSONException e) {
-                    e.printStackTrace();
+            Utilities.httpRequest(this, Request.Method.GET, "/groups?searchBy=ids&ids=" + id_group, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONArray user_response = new JSONArray((String) response);
+                        map.put("group_name",new JSONObject(user_response.getString(0)).getString("name"));
+                        System.out.println("----->"+user_response);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-        },new Response.ErrorListener(){
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(Creazione_evento.this, error.toString(), Toast.LENGTH_LONG).show();
-                System.err.println(error.getMessage());
-            }
-        },new HashMap<>());
-        String[] Hinizio = inizio.split(":");
-        String[] Hfine = fine.split(":");
-        HashMap<String,String> mapTime = new HashMap<>();
-        mapTime.put("summary",nome);
-        mapTime.put("location",luogo);
-        mapTime.put("start",DateStart);
-        mapTime.put("end",DateEnd);
-        mapTime.put("cost","");
-        mapTime.put("requiredChildren","2");
-        mapTime.put("groupId",id_group);
-        mapTime.put("startHour",Hinizio[0]);//--------
-        mapTime.put("link","");
-        mapTime.put("requiredParents","2");
-        mapTime.put("repetition","none");
-        mapTime.put("activityColor","#000000");
-        mapTime.put("children","[]");
-        mapTime.put("externals","[]");
-        mapTime.put("endHour",Hfine[0]);
-        mapTime.put("category","");
-        mapTime.put("status","ongoing");
-        mapTime.put("parents","[]");
-        System.out.println("----->"+mapTime);
-        Utilities.httpRequest(this, Request.Method.POST, "/groups/"+id_group+"/nekomaActivities", new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                JSONObject tmp = null;
-                try {
-                    tmp = new JSONObject(response);
-                    mapTime.put("activityId",tmp.getString("id"));
-                    System.out.println("----->"+mapTime);
-                    Utilities.httpRequest(Creazione_evento.this, Request.Method.POST, "/groups/"+id_group+"/nekomaActivities/"+tmp.getString("id")+"/date", new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            JSONObject tmp = null;
-                            try {
-                                tmp = new JSONObject(response);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
+            },new Response.ErrorListener(){
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(Creazione_evento.this, error.toString(), Toast.LENGTH_LONG).show();
+                    System.err.println(error.getMessage());
+                }
+            },new HashMap<>());
+            String[] Hinizio = inizio.split(":");
+            String[] Hfine = fine.split(":");
+            HashMap<String,String> mapTime = new HashMap<>();
+            mapTime.put("summary",nome);
+            mapTime.put("location",luogo);
+            mapTime.put("start",DateStart);
+            mapTime.put("end",DateEnd);
+            mapTime.put("cost","");
+            mapTime.put("requiredChildren","2");
+            mapTime.put("groupId",id_group);
+            mapTime.put("startHour",Hinizio[0]);
+            mapTime.put("link","");
+            mapTime.put("requiredParents","2");
+            mapTime.put("repetition","none");
+            mapTime.put("activityColor","#000000");
+            mapTime.put("children","[]");
+            mapTime.put("externals","[]");
+            mapTime.put("endHour",Hfine[0]);
+            mapTime.put("category","");
+            mapTime.put("status","ongoing");
+            mapTime.put("parents","[]");
+            Utilities.httpRequest(this, Request.Method.POST, "/groups/"+id_group+"/nekomaActivities", new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    JSONObject tmp = null;
+                    try {
+                        tmp = new JSONObject(response);
+                        mapTime.put("activityId",tmp.getString("id"));
+                        System.out.println("----->"+mapTime);
+                        Utilities.httpRequest(Creazione_evento.this, Request.Method.POST, "/groups/"+id_group+"/nekomaActivities/"+tmp.getString("id")+"/date", new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Intent homepageA = new Intent(Creazione_evento.this,Homepage.class);
+                                startActivity(homepageA);
                             }
-                        }
-                    },new Response.ErrorListener(){
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(Creazione_evento.this, error.toString(), Toast.LENGTH_LONG).show();
-                            System.err.println(error.getMessage());
-                        }
-                    },mapTime);
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                        },new Response.ErrorListener(){
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(Creazione_evento.this, error.toString(), Toast.LENGTH_LONG).show();
+                                System.err.println(error.getMessage());
+                            }
+                        },mapTime);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-        },new Response.ErrorListener(){
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(Creazione_evento.this, error.toString(), Toast.LENGTH_LONG).show();
-                System.err.println(error.getMessage());
-            }
-        },map);
+            },new Response.ErrorListener(){
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(Creazione_evento.this, error.toString(), Toast.LENGTH_LONG).show();
+                    System.err.println(error.getMessage());
+                }
+            },map);
+
+        }
+
 
 
 
