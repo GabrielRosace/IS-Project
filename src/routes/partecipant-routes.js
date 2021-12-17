662886148c22461b27fa81ef060a109dd5936042
osammot10 <tommasopellizzon2000@gmail.com>
2021-12-17 17:42:46 +0100

    update of partecipation dates

--- src/routes/partecipant-routes.js
+++ src/routes/partecipant-routes.js
@@ -87,4 +87,74 @@ router.post('/', (req, res, next) => {
     })
 })
 
+// Elimina una partecipazione
+router.delete('/:activity_id', (req, res, next) => {
+    let userId = req.user_id
+    if (!userId) { return res.status(401).send('Not authenticated') }
+
+    Partecipant.deleteOne({activity_id: req.params.activity_id, partecipant_id: userId}).exec()
+    return res.status(200).send('Partecipation deleted')
+})
+
+// Modifica una partecipazione
+router.patch('/:activity_id', (req, res, next) => {
+    let userId = req.user_id
+    if (!userId) { return res.status(401).send('Not authenticated') }
+
+    if(req.body.days){
+        let daysSplitted = req.body.days ? req.body.days.split(',') : undefined
+        let days = []
+    
+        for(let i = 0; i < daysSplitted.length; i++){
+            days.push(new Date(daysSplitted[i]))
+        }
+
+        Recurrence.findOne({activity_id: req.params.activity_id}).exec().then((r) => {
+            let valid = false
+            switch(r.type){
+                case 'daily':
+                    for(let i = 0; i < days.length; i++){                       
+                        if(days[i] < r.start_date[0] || days[i] > r.end_date[0]){
+                            return res.status(400).send('Incorrect days')
+                        }
+                    }
+                    break
+                
+                case 'weekly':
+                    valid = false
+                    for(let i = 0; i < days.length; i++){
+                        for(let j = 0; j < start_date.length; j++){
+                            if(days[i] < r.start_date[0] || days[i] > r.end_date[end_date.length - 1]){
+                                return res.status(400).send('Incorrect days')
+                            }
+                            if(days[i].getDay() == r.start_date[j].getDay()){
+                                valid = true
+                            }
+                            if(!valid) return res.status(400).send('Incorrect days')
+                        }
+                    }
+                    break
+
+                case 'monthly':
+                    valid = false
+                    for(let i = 0; i < days.length; i++){
+                        for(let j = 0; j < r.start_date.length; j++){
+                            console.log(days[i].getDate());
+                            console.log(r.start_date[j].getDate());
+                            if(days[i].getDate() == r.start_date[j].getDate()){
+                                valid = true
+                            }
+                            if(days[i] < r.start_date[0] || days[i] > r.end_date[r.end_date.length - 1]) return res.status(400).send('Incorrect days2')
+                        }
+                    }
+                    if(!valid) return res.status(400).send('Incorrect days1')
+                    break
+            }
+
+            Partecipant.updateOne({activity_id: r.activity_id, partecipant_id: userId},{ $set: {days: days} }).exec()
+        })
+
+    }
+})
+
 module.exports = router
