3ce1eacedc02dffc02b066dfdcd628faf3bcd996
bealice011 <869292@stud.unive.it>
2021-12-22 14:11:16 +0100

    add comments and bug fix

--- Nekoma_FamiliesShare/app/src/main/java/com/example/nekoma_families_share/SceltaCreazione.java
+++ Nekoma_FamiliesShare/app/src/main/java/com/example/nekoma_families_share/SceltaCreazione.java
@@ -15,6 +15,7 @@ public class SceltaCreazione extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scelta_creazione);
 
+        // permette di spostarsi con la navigation icon della toolbar
         Toolbar t = (Toolbar) findViewById(R.id.toolbar_scelta);
         t.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,14 +25,19 @@ public class SceltaCreazione extends AppCompatActivity {
         });
     }
 
+    // si sposta sulla schermata dell'evento
     public void getEvento(View v){
         Intent event = new Intent(SceltaCreazione.this,Creazione_evento.class);
         startActivity(event);
     }
+
+    // si sposta sulla schermata del servizio
     public void getServizio(View v){
         Intent servizio = new Intent(SceltaCreazione.this,NuovoServizio.class);
         startActivity(servizio);
     }
+
+    // si sposta sulla schermata di evento ricorrente
     public void getEventoRicorrent(View v){
         Intent event = new Intent(SceltaCreazione.this,Creazione_evento_ricorrente.class);
         startActivity(event);
--- Nekoma_FamiliesShare/app/src/main/java/com/example/nekoma_families_share/Utilities.java
+++ Nekoma_FamiliesShare/app/src/main/java/com/example/nekoma_families_share/Utilities.java
@@ -211,7 +211,7 @@ public class Utilities {
         }
 
         public myRecEvent(JSONObject obj) throws JSONException {
-            JSONObject recAct = null;
+/*            JSONObject recAct = null;
 //            System.out.println(obj);
             if(obj.has("partecipant")){
                 this.nPart = obj.getJSONObject("partecipant").length();
@@ -233,24 +233,28 @@ public class Utilities {
                 }
             
             } else {
-                this.nome = "";
-                this.img = "";
-                this.descrizione = "";
-                this.owner_id = "";
-                this.labels = "";
+                this.nome = obj.getString("name");
+                this.img = obj.getString("image_url");
+                this.descrizione = obj.getString("description");
+                this.owner_id = obj.getString("creator_id");
+                this.labels = obj.getString("labels");
             }
             this.event_id = obj.getString("activity_id");
             if(obj.has("RecurringActivity") && recAct.has("Recurrence")){
+                System.out.println("*********************lololololo");
                 recAct = recAct.getJSONArray("Recurrence").getJSONObject(0);
                 this.recType = recAct.getString("type");
                 this.start_date = recAct.getString("start_date");
                 this.end_date = recAct.getString("end_date");
             }else{
-                if(obj.has("type")){
-                    this.recType = obj.getString("type");
-                    this.start_date = obj.getString("start_date");
-                    this.end_date = obj.getString("end_date");
+                System.out.println("*********************lilililililiili");
+                if(recAct.has("type")){
+                    System.out.println("*********************hihihihihihihih");
+                    this.recType = recAct.getString("type");
+                    this.start_date = recAct.getString("start_date");
+                    this.end_date = recAct.getString("end_date");
                 }else{
+                    System.out.println("*********************aahhahahahhhahahahahahaah");
                     this.recType = "";
                     this.start_date = "";
                     this.end_date = "";
@@ -258,7 +262,48 @@ public class Utilities {
 
             }
 
-            this.enddate = ".";
+            this.enddate = ".";*/
+            JSONObject recAct = null;
+//            System.out.println(obj);
+            if(obj.has("partecipant")){
+                this.nPart = obj.getJSONObject("partecipant").length();
+                obj = obj.getJSONArray("event").getJSONObject(0);
+            }else{
+                this.nPart = 10;
+            }
+            if (obj.has("RecurringActivity") && !obj.getJSONArray("RecurringActivity").isNull(0)) {
+                recAct = obj.getJSONArray("RecurringActivity").getJSONArray(0).getJSONObject(0);
+                this.nome = recAct.getString("name");
+                this.img = recAct.getString("image_url");
+                this.descrizione = recAct.getString("description");
+                this.owner_id = recAct.getString("creator_id");
+                this.labels = recAct.getString("labels");
+            } else {
+                this.nome = obj.getString("name");
+                this.img = obj.getString("image_url");
+                this.descrizione = obj.getString("description");
+                this.owner_id = obj.getString("creator_id");
+                this.labels = obj.getString("labels");
+            }
+            if(obj.has("Recurrence")){
+                this.recType = obj.getJSONArray("Recurrence").getJSONObject(0).getString("type");
+                this.start_date = obj.getJSONArray("Recurrence").getJSONObject(0).getString("start_date");
+                this.end_date = obj.getJSONArray("Recurrence").getJSONObject(0).getString("end_date");
+                this.event_id = obj.getJSONArray("Recurrence").getJSONObject(0).getString("activity_id");
+                this.enddate = ".";
+            }else if(obj.has("RecurringActivity")){
+                this.recType = obj.getJSONArray("RecurringActivity").getJSONArray(0).getJSONObject(0).getJSONArray("Recurrence").getJSONObject(0).getString("type");
+                this.start_date = obj.getJSONArray("RecurringActivity").getJSONArray(0).getJSONObject(0).getJSONArray("Recurrence").getJSONObject(0).getString("start_date");
+                this.end_date = obj.getJSONArray("RecurringActivity").getJSONArray(0).getJSONObject(0).getJSONArray("Recurrence").getJSONObject(0).getString("end_date");
+                this.event_id = obj.getJSONArray("RecurringActivity").getJSONArray(0).getJSONObject(0).getJSONArray("Recurrence").getJSONObject(0).getString("activity_id");
+                this.enddate = ".";
+            }else{
+                this.recType = obj.getString("type");
+                this.start_date = obj.getString("start_date");
+                this.end_date = obj.getString("end_date");
+                this.event_id = obj.getString("activity_id");
+                this.enddate = ".";
+            }
         }
 
         public myRecEvent(String s) {
